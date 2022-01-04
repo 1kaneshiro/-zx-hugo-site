39db6c77ac217c9902b36372e653099b558d00d3
1kaneshiro <q2486048058@163.com>
2022-01-05 00:10:42 +0800

    update:2022/01/05 周三

--- scripts/win_build_and_update.bat
+++ scripts/win_build_and_update.bat
@@ -1,5 +1,6 @@
 :: rmdir /s 删除目录树 /q 安静模式
-rmdir /s /q docs && ^
+:: 此脚本在根目录执行
+if exist docs (rmdir /s /q docs) else (echo "docs not exists") && ^
 docker run --name my-hugo -v D:\Coding\hugo-site:/src -p 1313:1313 --rm -it klakegg/hugo && ^
 git add -A  && ^
 git commit -m "update:%date%" && ^
