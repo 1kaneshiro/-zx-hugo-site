96a63054c52cdbfef73358c645f7da0cd63ecbe8
1kaneshiro <q2486048058@163.com>
2022-01-09 22:49:23 +0800

    update:20220109

--- scripts/mac_build_and_update.sh
+++ scripts/mac_build_and_update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -rf docs/ &&
 docker run --name my-hugo -v /Users/zzx/Project/site:/src -p 1313:1313 --rm -it klakegg/hugo &&
 git add -A &&
 git commit -m "update:$(date +%Y%m%d)" &&
--- scripts/mac_run_server.sh
+++ scripts/mac_run_server.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 docker run --name my-hugo -v /Users/zzx/Project/site:/src -p 1313:1313 --rm -it klakegg/hugo server
